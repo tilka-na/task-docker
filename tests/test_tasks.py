ca5b3dad212fb66c29e127dee25ccbc9440ac560
tilka.na <nadia.arrahime@gmail.com>
2026-03-16 11:55:43 +0100

    initial commmit

---
+++ tests/test_tasks.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../app')))
+def test_add_task():
+    add_task("Learn Docker")
+    assert "Learn Docker" in get_tasks()
+def test_multiple_tasks():
+    add_task("Learn CI")
+    add_task("Learn DevOps")
+    tasks = get_tasks()
+    assert "Learn CI" in tasks
+    assert "Learn DevOps" in tasks
